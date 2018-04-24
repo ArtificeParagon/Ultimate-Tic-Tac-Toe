fc09909fbd5428daa75bf16a066d7d0d8753f629
LappGod <watertower5132@gmail.com>
2018-04-23 19:52:55 -0500

    Canvas created

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -4,19 +4,27 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Client extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        VBox root = new VBox();
+        HBox root = new HBox();
         primaryStage.setTitle("Ultimate Tic Tac Toe");
 
         Canvas canvas = new Canvas();
+        canvas.setWidth(600);
+        canvas.setHeight(600);
         GraphicsContext gc = canvas.getGraphicsContext2D();
+        gc.setFill(Color.GREEN);
+        gc.fillRect(0,0,gc.getCanvas().getWidth(),gc.getCanvas().getHeight());
 
+
+
+        root.getChildren().addAll(canvas);
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
